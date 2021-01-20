fea6a260ae74a00e66d8d70e64f2d8ff9cebb8a9
Sahas Saurav <mike.sahas@gmail.com>
2021-01-20 18:28:32 +0530

    cors problem

--- sw.js
+++ sw.js
@@ -33,7 +33,9 @@ self.addEventListener("active", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((resp)=>{
-      return resp || fetch(event.request).then((response)=>{
+      return resp || fetch(event.request,{
+        mode:'cors'
+      }).then((response)=>{
         let responseClone=response.clone();
         caches.open(cacheName).then((cache)=>{
           cache.put(event.request,responseClone)
