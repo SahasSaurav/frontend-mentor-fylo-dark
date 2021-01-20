fd7d030c6772231ab88e03ea87ef1a989d9f27ad
Sahas Saurav <mike.sahas@gmail.com>
2021-01-20 18:21:37 +0530

    added service worker for offline compatbility

---
+++ sw.js
@@ -0,0 +1,45 @@
+const version = "v1";
+const cacheName = `sw-${version}`;
+const preCacheAssets = [
+  "/",
+  "./index.html",
+  "./css/main.css",
+  "./js/app.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(preCacheAssets);
+    })
+  );
+});
+
+self.addEventListener("active", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheName) => {
+      return Promise.all(
+        cacheName.map((cache) => {
+          if (cache !== cacheName) {
+            console.log(`Service Worker: Clearing Old Cache`);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp)=>{
+      return resp || fetch(event.request).then((response)=>{
+        let responseClone=response.clone();
+        caches.open(cacheName).then((cache)=>{
+          cache.put(event.request,responseClone)
+        });
+        return response
+      })
+    })
+  )
+});
